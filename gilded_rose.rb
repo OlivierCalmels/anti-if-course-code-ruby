f0af6a86c051830b35a41e7dceeaa1a5451a98a3
OlivierCalmels <61502515+OlivierCalmels@users.noreply.github.com>
2023-01-03 17:39:30 +0100

    Extract Sulfuras - even though it's empty class

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -73,6 +73,18 @@ class GildedRose
     end
   end
 
+  # Sulfuras class
+  class Sulfuras
+    attr_reader :quality, :sell_in
+
+    def initialize(quality, sell_in)
+      @quality, @sell_in = quality, sell_in
+    end
+
+    def update
+    end
+  end
+
   def initialize(items)
     @items = items
   end
@@ -80,6 +92,10 @@ class GildedRose
   def update_quality
     @items.each do |item|
       if sulfuras?(item)
+        sulfuras = Sulfuras.new(item.quality, item.sell_in)
+        sulfuras.update
+        item.quality = sulfuras.quality
+        item.sell_in = sulfuras.sell_in
       elsif generic?(item)
         generic = Generic.new(item.quality, item.sell_in)
         generic.update
