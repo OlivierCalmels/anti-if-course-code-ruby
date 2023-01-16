9d5beabcc713097b7d7fed3659424452e90df029
OlivierCalmels <61502515+OlivierCalmels@users.noreply.github.com>
2023-01-16 15:45:54 +0100

    The trick to allow mutant run on 2 modules

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,100 +1,99 @@
 # frozen_string_literal:true
 
-module Inventory
-  # Quality class
-
-  class Quality
-    attr_reader :amount
-
-    def initialize(amount)
-      @amount = amount
-    end
-
-    def degrade
-      @amount -= 1 if amount > 0
-    end
-
-    def increase
-      @amount += 1 if amount < 50
-    end
-
-    def reset
-      @amount = 0
-    end
-  end
-
-  # Generic class
-  class Generic
-    attr_reader :sell_in
-
-    def initialize(quality, sell_in)
-      @quality, @sell_in = Quality.new(quality), sell_in
-    end
-
-    def quality
-      @quality.amount
-    end
-
-    def update
-      @quality.degrade
-      @sell_in = sell_in - 1
-      if sell_in < 0
-        @quality.degrade
-      end
-    end
-  end
-
-  # AgedBrie class
-  class AgedBrie
-    attr_reader :sell_in
-
-    def initialize(quality, sell_in)
-      @quality, @sell_in = Quality.new(quality), sell_in
-    end
-
-    def quality
-      @quality.amount
-    end
-
-    def update
-      @quality.increase
-        @sell_in = sell_in - 1
-      if sell_in < 0
-        @quality.increase
-      end
-    end
-  end
-
-  # BackstagePass class
-  class BackstagePass
-    attr_reader :sell_in
-
-    def initialize(quality, sell_in)
-      @quality, @sell_in = Quality.new(quality), sell_in
-    end
-
-    def quality
-      @quality.amount
-    end
-
-    def update
-      @quality.increase
-      if sell_in < 11
-        @quality.increase
-      end
-      if sell_in < 6
-        @quality.increase
-      end
-      @sell_in = sell_in - 1
-      if sell_in < 0
-        @quality.reset
-      end
-    end
-  end
-end
-
 # GildedRose class
 class GildedRose
+  module Inventory
+    # Quality class
+    class Quality
+      attr_reader :amount
+
+      def initialize(amount)
+        @amount = amount
+      end
+
+      def degrade
+        @amount -= 1 if amount > 0
+      end
+
+      def increase
+        @amount += 1 if amount < 50
+      end
+
+      def reset
+        @amount = 0
+      end
+    end
+
+    # Generic class
+    class Generic
+      attr_reader :sell_in
+
+      def initialize(quality, sell_in)
+        @quality, @sell_in = Quality.new(quality), sell_in
+      end
+
+      def quality
+        @quality.amount
+      end
+
+      def update
+        @quality.degrade
+        @sell_in = sell_in - 1
+        if sell_in < 0
+          @quality.degrade
+        end
+      end
+    end
+
+    # AgedBrie class
+    class AgedBrie
+      attr_reader :sell_in
+
+      def initialize(quality, sell_in)
+        @quality, @sell_in = Quality.new(quality), sell_in
+      end
+
+      def quality
+        @quality.amount
+      end
+
+      def update
+        @quality.increase
+        @sell_in = sell_in - 1
+        if sell_in < 0
+          @quality.increase
+        end
+      end
+    end
+
+    # BackstagePass class
+    class BackstagePass
+      attr_reader :sell_in
+
+      def initialize(quality, sell_in)
+        @quality, @sell_in = Quality.new(quality), sell_in
+      end
+
+      def quality
+        @quality.amount
+      end
+
+      def update
+        @quality.increase
+        if sell_in < 11
+          @quality.increase
+        end
+        if sell_in < 6
+          @quality.increase
+        end
+        @sell_in = sell_in - 1
+        if sell_in < 0
+          @quality.reset
+        end
+      end
+    end
+  end
+
   # GoodCategory class
   class GoodCategory
     def build_for(item)
@@ -129,6 +128,7 @@ class GildedRose
   def update_quality
     @items.each do |item|
       next if sulfuras?(item)
+
       good = GoodCategory.new.build_for(item)
       good.update
       item.quality = good.quality
